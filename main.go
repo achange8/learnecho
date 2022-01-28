8f20dad5b65d49846df20e85da3477ce40f1f5ad
unknown <achange888@gmail.com>
2022-01-28 18:26:59 +0900

    123

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/achange8/learnecho/router"
 )
 
 func main() {
 	fmt.Println("Welcome osh server with echo!")
+	fmt.Println(os.Getenv("DBNAME"))
 	e := router.New()
-
 	e.Logger.Fatal(e.Start(":8080"))
 }
